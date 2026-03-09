48b555d1bb2705685b97e09a27ea878347968973
Prathamkacher <pratham9340@gmail.com>
2026-03-09 12:06:31 +0530

    [Pratham]: added UC16-Ability to rerieve all the Entries from the DB

--- src/test/java/com/addressbook/AddressBookSystemApplicationTests.java
+++ src/test/java/com/addressbook/AddressBookSystemApplicationTests.java
@@ -1,13 +1,27 @@
 package com.addressbook;
 
+import com.addressbook.model.Contact;
+import com.addressbook.service.DatabaseService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AddressBookSystemApplicationTests {
+import java.util.List;
 
-	@Test
-	void contextLoads() {
-	}
+import static org.junit.jupiter.api.Assertions.*;
 
+public class AddressBookSystemApplicationTests {
+
+    DatabaseService databaseService = new DatabaseService();
+
+    @Test
+    void testRetrieveContactsFromDatabase() {
+
+        List<Contact> contacts =
+                databaseService.getAllContacts();
+
+        assertNotNull(contacts);
+
+        System.out.println("\nContacts Retrieved From Database:\n");
+
+        contacts.forEach(System.out::println);
+    }
 }
